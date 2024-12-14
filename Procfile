957d676d7507b3fd8e1c816e7a93ec9a4ea2dda5
unknown <huynhbalehuytr@gmail.com>
2024-12-14 17:36:45 +0700

    update

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn cinema.wsgi --log-file -
+web: python manage.py migrate && gunicorn cinema.wsgi
--- cinema/settings.py
+++ cinema/settings.py
@@ -80,12 +80,25 @@ WSGI_APPLICATION = 'cinema.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#             'NAME' : 'railway',
+#             'USER' : 'postgres',
+#             'PASSWORD': 'rfKHjoxxjqfPXpufkoBxBXkeZxRoJXQd',
+#             'HOST': 'postgres.railway.internal',
+#             'PORT': '5432',
+#         }
+#     } 
+
+
 
 
 # Password validation
